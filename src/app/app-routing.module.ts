0cb0a6dd52c973da39e1fad529351f234e07089c
Ismael SAI <ismaelsai@yahoo.com>
2022-08-02 10:22:41 +0200

    exo 2 fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { TransactionsModule } from './transactions/transactions.module';
-
+import { BeneficiaireModule } from './beneficiaire/beneficiaire.module';
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '',
-    loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule)
+    loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsModule),
+  },
+  { path: '',
+  loadChildren: () => import('./beneficiaire/beneficiaire.module').then(m=>m.BeneficiaireModule)
   },
   { path: 'welcome', component: WelcomeComponent },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -16,7 +19,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    
+
   ],
   imports: [
     TransactionsModule,
